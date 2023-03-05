920bb1768e6089c5e1be9976625ed4e4677ab427
Tanner <tmartzdc@gmail.com>
2023-03-05 12:45:07 -0600

    3.5.23.1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """
 CMPS 2200  Assignment 2.
 See assignment-02.pdf for details.
+Tanner Martz v3.4.23-04
 """
 import time
 
@@ -43,22 +44,73 @@ def pad(x,y):
         y = ['0'] + y
     return x,y
 
-
-
 def subquadratic_multiply(x, y):
-    ### TODO
-    pass
-    ###
+    ### TOD0:
+
+    """
+    Multiplies two BinaryNumber objects using a divide-and-conquer approach.
+    Assumes x and y are binary vectors of equal length with an even number of bits.
+    """
+    # Get binary vectors of x and y
+    x_vec = x.binary_vec
+    y_vec = y.binary_vec
+    
+    # Base case: if x and y are single bits, return their product
+    if x.decimal_val <= 1 and y.decimal_val <= 1:
+        return BinaryNumber(x.decimal_val * y.decimal_val)
+    
+    # Pad x and y with leading zeros to ensure they have the same number of bits
+    x_vec, y_vec = pad(x_vec, y_vec)
+    
+    # Get the length of the binary vectors
+    length = len(x_vec)
+    
+    # Split x and y into left and right halves
+    x_L, x_R = split_number(x_vec)
+    y_L, y_R = split_number(y_vec)
+    
+    # Recursively compute the products of the left and right halves
+    left = subquadratic_multiply(x_L, y_L)
+    right = subquadratic_multiply(x_R, y_R)
+    
+    # Compute the sum of the left and right halves of x and y
+    x_sum = BinaryNumber(x_L.decimal_val + x_R.decimal_val)
+    y_sum = BinaryNumber(y_L.decimal_val + y_R.decimal_val)
+    
+    # Compute the product of the sums and subtract the products of the left and right halves
+    mid_opp = subquadratic_multiply(x_sum, y_sum).decimal_val 
+    mid = BinaryNumber(mid_opp - left.decimal_val - right.decimal_val)
+    
+    # Bit-shift the middle product to the right position
+    mid = bit_shift(mid, length//2) 
+    left = bit_shift(left, length)
+    
+    # Compute the final product as the sum of the three products
+    return BinaryNumber(left.decimal_val + mid.decimal_val + right.decimal_val)
+  
 
 ## Feel free to add your own tests here.
 def test_multiply():
-    assert subquadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert subquadratic_multiply(BinaryNumber(2), BinaryNumber(2)).decimal_val == 2*2
+    assert subquadratic_multiply(BinaryNumber(3), BinaryNumber(4)).decimal_val == 3*4
+    assert subquadratic_multiply(BinaryNumber(5), BinaryNumber(6)).decimal_val == 5*6
+    assert subquadratic_multiply(BinaryNumber(0), BinaryNumber(1)).decimal_val == 0*1
 
 def time_multiply(x, y, f):
     start = time.time()
     # multiply two numbers x, y using function f
+    f(BinaryNumber(x), BinaryNumber(y))
     return (time.time() - start)*1000
 
-    
-    
-
+if __name__ == "__main__":
+    print("start tests.")
+    # Test subquadratic_multiply function
+    print("test_multiply():")
+    test_multiply()
+    # Test the performance of subquadratic_multiply function on a range of inputs
+    print("time_multiply():")
+    test_range = list(map(lambda n: 2**n, range(0,1000,100)))
+    for test in test_range:
+        t = time_multiply(test, test, subquadratic_multiply)
+        print("test {:.2e} * {:.2e} in {:.8f}ms".format(test, test, t))
+    print("tests complete.")
